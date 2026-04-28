0e6bd52397ad1dc1e89816460d716c18d71d5deb
arizacris2431-hash <arizacris2431@gmail.com>
2026-04-28 15:46:50 -0500

    Taller de colecciones

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        // Lista de Tareas
 
         List<String> listaTareas = new ArrayList<>();
 
@@ -18,7 +20,41 @@ public class Main {
 
         mostrarTareasPendientes(listaTareas);
 
+
+        // Sets Libros
+
+        Set<String> libros = new HashSet<>();
+
+        agregarLibro(libros, "Harry Potter");
+        agregarLibro(libros, "El Principito");
+        agregarLibro(libros, "Clean Code");
+        agregarLibro(libros, "Harry Potter"); // duplicado (NO se agrega)
+
+        mostrarLibros(libros);
+
+        verificarLibro(libros, "El Principito");
+
+        contarLibros(libros);
+
+        // Maps
+
+        Map<String, Double> trabajadores = new HashMap<>();
+
+        agregarTrabajador(trabajadores, "Juan", 2000);
+        agregarTrabajador(trabajadores, "Ana", 2500);
+        agregarTrabajador(trabajadores, "Pedro", 1800);
+
+        mostrarTrabajadores(trabajadores);
+
+        actualizarSalario(trabajadores, "Juan", 3000);
+
+        mostrarTrabajadores(trabajadores);
+
+        calcularPromedio(trabajadores);
     }
+
+
+    // metodos Listas
 
     public static void agregarTarea(List<String> lista, String tarea) {
         lista.add(tarea);
@@ -50,5 +86,70 @@ public class Main {
         for (String l : lista) {
             System.out.println("- " + l);
         }
+    }
+
+    // metodos setos
+
+    public static void agregarLibro(Set<String> libros, String libro) {
+        libros.add(libro);
+    }
+
+    public static void mostrarLibros(Set<String> libros) {
+        System.out.println("\n===== Libros disponibles =====");
+        for (String l : libros) {
+            System.out.println("- " + l);
+        }
+    }
+
+    public static void verificarLibro(Set<String> libros, String libro) {
+        System.out.println("\n===== Verificar Libros =====");
+        if (libros.contains(libro)) {
+            System.out.println("El libro '" + libro + "' está disponible");
+        } else {
+            System.out.println("El libro '" + libro + "' NO está disponible");
+        }
+    }
+
+    public static void contarLibros(Set<String> libros) {
+        System.out.println("\nTotal de libros: " + libros.size());
+    }
+
+
+    // metodos maps
+
+    public static void agregarTrabajador(Map<String, Double> map, String nombre, double salario) {
+        map.put(nombre, salario);
+    }
+
+    public static void mostrarTrabajadores(Map<String, Double> map) {
+
+        System.out.println("\n===== TRABAJADORES =====");
+
+        for (String nombre : map.keySet()) {
+            System.out.println(nombre + " -> $" + map.get(nombre));
+        }
+    }
+
+    public static void actualizarSalario(Map<String, Double> map, String nombre, double nuevoSalario) {
+
+        if (map.containsKey(nombre)) {
+            map.put(nombre, nuevoSalario);
+            System.out.println("Salario actualizado de " + nombre);
+        } else {
+            System.out.println("Trabajador no encontrado");
+        }
+    }
+
+    public static void calcularPromedio(Map<String, Double> map) {
+
+        double suma = 0;
+
+        for (double salario : map.values()) {
+            suma += salario;
+        }
+
+        double promedio = suma / map.size();
+
+        System.out.println("\nPromedio de salarios: " + promedio);
     }
 }
